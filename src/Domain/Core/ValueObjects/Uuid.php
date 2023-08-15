4c66f43c151abe07a110bf776ee70042f46659b9
Marcelo Fabiano <marcelofabianov@gmail.com>
2023-08-15 20:05:21 -0300

    feat: criado Uuid value object core domain

--- src/Domain/Core/ValueObjects/Uuid.php
+++ src/Domain/Core/ValueObjects/Uuid.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace FurryFinds\Domain\Core\ValueObjects;
 
+use FurryFinds\Domain\Core\Exceptions\FurryDomainCoreException;
 use FurryFinds\Domain\Core\Interfaces\ValueObjects\IUuid;
+use Ramsey\Uuid\Uuid as RamseyUuid;
 
 final readonly class Uuid implements IUuid
 {
@@ -15,11 +17,46 @@ final readonly class Uuid implements IUuid
 
     public function __toString(): string
     {
-        return '';
+        return $this->value;
     }
 
     public function getValue(): string
     {
         return $this->value;
+    }
+
+    public function equals(IUuid $other): bool
+    {
+        return $this->getValue() === $other->getValue();
+    }
+
+    public static function random(): IUuid
+    {
+        return new self((string) RamseyUuid::uuid4());
+    }
+
+    public static function validate(string $value): bool
+    {
+        if ($value === '') {
+            return false;
+        }
+
+        return RamseyUuid::isValid($value);
+    }
+
+    /**
+     * @throws FurryDomainCoreException
+     */
+    public static function create(string|IUuid $value): IUuid
+    {
+        if ($value instanceof IUuid) {
+            return $value;
+        }
+
+        if (! self::validate($value)) {
+            throw FurryDomainCoreException::invalidArgument('Invalid UUID');
+        }
+
+        return new self($value);
     }
 }
